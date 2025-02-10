6ff8adcad462dd81e8de4b49c75c8775af900084
kineze <krishantharaka@outlook.com>
2025-02-10 11:11:00 +0530

    add phone verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',  
+        'phone_verified',
     ];
 
     /**
@@ -64,6 +66,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'phone_verified' => 'boolean',
         ];
     }
 }
